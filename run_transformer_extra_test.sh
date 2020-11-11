8269e289a67c946c10ef6f7469df8798c4a8d8ba
Mats Sjöberg <mats.sjoberg@csc.fi>
2020-11-11 10:50:54 +0000

    Add single script for evaluating extra testset

---
+++ run_transformer_extra_test.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+DATASET=$1
+LABEL_EMB=$2    # pifa-tfidf | pifa-neural | text-emb
+MODEL_TYPE=$3
+TESTX=$4
+MAX_XSEQ_LEN=$5
+GPID=$6
+MODEL_EXTRA=$7  #-30000
+
+DATA_DIR=datasets
+OUTPUT_DIR=save_models/${DATASET}
+PROC_DATA_DIR=${OUTPUT_DIR}/proc_data
+DATASET_DIR="${DATA_DIR}/${DATASET}"
+
+if [ -z "$LABEL_EMB" -o -z "$TESTX" ]; then
+    echo "Usage: $0 DATASET LABEL_EMB MODEL_TYPE TESTX [MAX_XSEQ_LEN] [GPID] [MODEL_EXTRA]"
+    echo
+    echo "Example: $0 yso-en pifa-tfidf bert x 128 0,1 -30000"
+    exit 1
+fi
+
+if [ -z "$MAX_XSEQ_LEN" ]; then
+    MAX_XSEQ_LEN=128
+    echo "Setting MAX_XSEQ_LEN=$MAX_XSEQ_LEN"
+fi
+
+if [ -z "$GPID" ]; then
+    GPID=$(nvidia-smi -L | cut -d : -f 1 | cut -d \  -f 2 | tr '\n' ,)
+    GPID=${GPID::-1}
+    echo "Setting GPID=$GPID"
+fi
+    
+if [ ! -d ${DATASET_DIR} ]; then
+    echo "Could not find dataset in directory ${DATASET_DIR}!"
+    exit 1
+fi
+
+# HuggingFace pretrained model preprocess
+if [ $MODEL_TYPE == "bert" ]; then
+    MODEL_NAME="bert-large-cased-whole-word-masking"
+elif [ $MODEL_TYPE == "roberta" ]; then
+    MODEL_NAME="roberta-large"
+elif [ $MODEL_TYPE == 'xlnet' ]; then
+    MODEL_NAME="xlnet-large-cased"
+elif [ $MODEL_TYPE == 'bert-multilingual' ]; then
+    MODEL_NAME="bert-base-multilingual-uncased"
+else
+    echo "Unknown MODEL_NAME!"
+    exit 1
+fi
+
+# construct C.tsx.[label-emb].npz for training matcher
+echo "construct C.tsx.[label-emb].npz for training matcher"
+SEED=0
+LABEL_EMB_NAME=${LABEL_EMB}-s${SEED}
+INDEXER_DIR=${OUTPUT_DIR}/${LABEL_EMB_NAME}/indexer
+python -u -m xbert.preprocess \
+    --do_proc_label --extra_test $TESTX \
+    -i ${DATASET_DIR} \
+    -o ${PROC_DATA_DIR} \
+    -l ${LABEL_EMB_NAME} \
+    -c ${INDEXER_DIR}/code.npz
+
+# do_proc_feat
+echo "do_proc_feat"
+OUTPUT_DIR=save_models/${DATASET}
+PROC_DATA_DIR=${OUTPUT_DIR}/proc_data
+mkdir -p ${PROC_DATA_DIR}
+python -u -m xbert.preprocess \
+    --do_proc_feat --extra_test $TESTX \
+    -i ./datasets/${DATASET} \
+    -o ${PROC_DATA_DIR} \
+    -m ${MODEL_TYPE} \
+    -n ${MODEL_NAME} \
+    --max_xseq_len ${MAX_XSEQ_LEN} \
+    |& tee ${PROC_DATA_DIR}/log.${MODEL_TYPE}.${MAX_XSEQ_LEN}.txt
+
+
+###############################################################################
+# Stuff from run_transformer_train_testx
+###############################################################################
+
+INDEXER_NAME=${LABEL_EMB_NAME}
+
+# Nvidia 2080Ti (11Gb), fp32
+#PER_DEVICE_VAL_BSZ=16
+
+# Nvidia V100 (16Gb), fp32
+PER_DEVICE_VAL_BSZ=32
+
+MODEL_DIR=${OUTPUT_DIR}/${INDEXER_NAME}/matcher${MODEL_EXTRA}/${MODEL_NAME}
+mkdir -p ${MODEL_DIR}
+
+# predict
+echo "predict"
+CUDA_VISIBLE_DEVICES=${GPID} python -u xbert/transformer.py \
+    -m ${MODEL_TYPE} -n ${MODEL_NAME} \
+    --do_eval -o ${MODEL_DIR} --extra_test $TESTX \
+    -x_trn ${PROC_DATA_DIR}/X.trn.${MODEL_TYPE}.${MAX_XSEQ_LEN}.pkl \
+    -c_trn ${PROC_DATA_DIR}/C.trn.${INDEXER_NAME}.npz \
+    -x_tst ${PROC_DATA_DIR}/X.ts${TESTX}.${MODEL_TYPE}.${MAX_XSEQ_LEN}.pkl \
+    -c_tst ${PROC_DATA_DIR}/C.ts${TESTX}.${INDEXER_NAME}.npz \
+    --per_device_eval_batch_size ${PER_DEVICE_VAL_BSZ}
+
+
+###############################################################################
+# Stuff from run_transformer_predict_testx
+###############################################################################
+
+LABEL_NAME_ARR=( $LABEL_EMB_NAME )
+MODEL_NAME_ARR=( $MODEL_NAME )
+EXP_NAME=${DATASET}.final
+
+PRED_NPZ_PATHS=""
+for LABEL_NAME in "${LABEL_NAME_ARR[@]}"; do
+    echo $LABEL_NAME
+    OUTPUT_DIR=save_models/${DATASET}/${LABEL_NAME}
+    INDEXER_DIR=${OUTPUT_DIR}/indexer
+    for MODEL_NAME in "${MODEL_NAME_ARR[@]}"; do
+        echo $MODEL_NAME
+        MATCHER_DIR=${OUTPUT_DIR}/matcher${MODEL_EXTRA}/${MODEL_NAME}
+        RANKER_DIR=${OUTPUT_DIR}/ranker${MODEL_EXTRA}/${MODEL_NAME}
+        
+        # predict final label ranking
+        echo "predict final label ranking"
+        PRED_NPZ_PATH=${RANKER_DIR}/ts${TESTX}.pred.npz
+        python -m xbert.ranker predict \
+            -m ${RANKER_DIR} -o ${PRED_NPZ_PATH} \
+            -x1 ${DATASET_DIR}/X.ts${TESTX}.npz \
+            -x2 ${MATCHER_DIR}/ts${TESTX}_embeddings.npy \
+            -y ${DATASET_DIR}/Y.ts${TESTX}.npz \
+            -z ${MATCHER_DIR}/C_ts${TESTX}_pred.npz \
+            -f 0 -t noop
+        
+        # append all prediction path
+        PRED_NPZ_PATHS="${PRED_NPZ_PATHS} ${PRED_NPZ_PATH}"
+    done
+done
+
+# final eval
+EVAL_DIR=results_transformer-large
+mkdir -p ${EVAL_DIR}
+python -u -m xbert.evaluator \
+    -y ${DATASET_DIR}/Y.ts${TESTX}.npz \
+    -e -p ${PRED_NPZ_PATHS} \
+    |& tee ${EVAL_DIR}/${EXP_NAME}.txt
+
